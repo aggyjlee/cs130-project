c9f95291d3d6edbe605cd27732e92c60ff886bee
Agnes Lee <agneslee@Agness-iMac.local>
2021-06-06 01:00:47 -0500

    updating email form

--- portfolio/mail.php
+++ portfolio/mail.php
@@ -2,7 +2,7 @@
 $email = $_POST['email'];
 $message = $_POST['message'];
 $formcontent="From: $name \n Message: $message";
-$recipient = "emailaddress@here.com";
+$recipient = "agneslee2022@u.northwestern.edu";
 $subject = "Contact Form";
 $mailheader = "From: $email \r\n";
 mail($recipient, $subject, $formcontent, $mailheader) or die("Error!");
